96e2ff30b344d8bf32949735f2ab8d2b987addf2
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-06-16 16:58:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,8 @@ class Song
   end
   
   def self.find_by_name(name)
-    answer = self.all.select {|song| song.name == name}
-    answer
+    song = self.all.select {|song| song.name == name}
+    song [0]
   end
   
   def self.find_or_create_by_name(name)
